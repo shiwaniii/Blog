1a99c51e6a76954de17af2767ef0092f6822665e
shiwaniii <Shiwani.adhikari@gmail.com>
2025-01-15 11:42:38 +0545

    added nav bar

--- Blog/settings.py
+++ Blog/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_FILEDIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
