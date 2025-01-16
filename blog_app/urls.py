6df8712b73a62f22acaf7d302d72d704f9314605
shiwaniii <Shiwani.adhikari@gmail.com>
2025-01-16 10:10:52 +0545

    changes in login

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post-detail/<int:pk>/', views.post_detail, name='post-detail'),
+    path('draft-list/<int:pk>/', views.draft-list, name='draft-list'),
+    path('draft-detail/<int:pk>/', views.draft-detail, name='draft-detail'),
 ]
 
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from blog_app.models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,3 +12,14 @@ def post_detail(request, pk):
 def post_list(request):
     post = Post.objects.filter(published_at__null=False).order_by("-published_at") #Show only publish data
     return render(request, "about.html", {"post": post})
+
+@login_required
+def draft_list(request):
+    post = Post.objects.get(pk=pk, published_at__isnull=True)
+    return render(request, "post_detail.html", {"post": post})
+
+@login_required
+def draft_list(request):
+    post = Post.objects.filter(published_at__null=True).order_by("-published_at") #Show only publish data
+    return render(request, "about.html", {"post": post})
+
