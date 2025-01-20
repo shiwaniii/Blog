d7ccab777b0f7952b7b7155fe5f8e29d57480b03
shiwaniii <Shiwani.adhikari@gmail.com>
2025-01-20 09:57:53 +0545

    added create and update view

--- blog_app/views.py
+++ blog_app/views.py
@@ -5,8 +5,9 @@ from django.utils import timezone
 
 from blog_app.forms import PostForm
 from blog_app.models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 class PostListView(ListView):
     model = Post
@@ -34,7 +35,38 @@ class DraftListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
      queryset = Post.objects.filter(published_at__isnull=True)
      return queryset
+    
+class DraftDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+    template_name = 'draft_detail.html'
+    context_object_name = "posts"
 
+    def get_queryset(self):
+     queryset = Post.objects.filter(published_at__isnull=True)
+     return queryset
+    
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+    sucess_url = reverse_lazy("post-list")
+
+    def form_valid(self, form):
+     form.instance.author = self.request.user
+     return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'post_create.html'
+    form_class = PostForm
+   
+
+    def get_success_url(self):
+     post = self.get_object()
+     if post.publishedat:
+        return reverse_lazy("post-detail", kwargs={"pk": post.pk})
+     else:
+        return reverse_lazy("draft-detail", kwargs={"pk": post.pk})
     
 
 
